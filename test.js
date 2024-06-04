7d9a74003c47955f6754d68bd2e745819a5685d7
varvara <kirylchyk@gmail.com>
2024-06-04 12:23:05 -0400

    cleanup

---
+++ test.js
@@ -0,0 +1,13 @@
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+    age() {
+        const date = new Date();
+        return date.getFullYear() - this.year;
+    }
+}
+
+const myCar = new Car("Ford", 2014);
+console.log(myCar.age())
